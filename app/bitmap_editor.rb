510f86cd68e68e224aecbcb29718287ca01280a1
Hemal Varambhia <hvarambhia@bookingbug.com>
2018-01-21 16:02:42 +0000

    Skeleton code for the session.

--- app/bitmap_editor.rb
+++ app/bitmap_editor.rb
@@ -1,15 +1,5 @@
 class BitmapEditor
-  def run(file)
-    return puts "please provide correct file" if file.nil? || !File.exists?(file)
+  def run(command)
 
-    File.open(file).each do |line|
-      line = line.chomp
-      case line
-      when 'S'
-          puts "There is no image"
-      else
-          puts 'unrecognised command :('
-      end
-    end
   end
 end
