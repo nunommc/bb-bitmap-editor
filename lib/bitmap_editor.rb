7629bb0682619c56e67ae6ceb804ede8f338a101
Adam Wenham <adamwenham64@gmail.com>
2018-02-01 08:32:19 +0000

    renamed app to lib, added initial spec for LXYC command

--- lib/bitmap_editor.rb
+++ lib/bitmap_editor.rb
@@ -7,6 +7,17 @@ class BitmapEditor
 
   def run(command)
     split_command = command.split(" ")
+    case split_command[0]
+    when 'C'
+      create(split_command)
+    when 'L'
+      add_colour(split_command)
+    else
+      return "Unknown command"
+    end
+  end
+
+  def create
     width = split_command[1].to_i
     height = split_command[2].to_i
     image.create({width: width, height: height})
--- spec/bitmap_editor_spec.rb
+++ spec/bitmap_editor_spec.rb
@@ -1,24 +1,23 @@
 require_relative '../app/bitmap_editor'
 
 describe BitmapEditor do
+  let(:image) { double(:image) }
+
   describe "I M N" do
     context "with a well formatted command" do
       it "creates a 2x2 white image" do
-        image = double :image
         expect(image).to receive(:create).with(width: 2, height: 2)
 
         BitmapEditor.new(image).run('I 2 2')
       end
 
       it "creates a 3x3 white image" do
-        image = double :image
         expect(image).to receive(:create).with(width: 3, height: 3)
 
         BitmapEditor.new(image).run('I 3 3')
       end
 
       it "creates a 3x9 white image" do
-        image = double :image
         expect(image).to receive(:create).with(width: 3, height: 9)
 
         BitmapEditor.new(image).run('I 3 9')
@@ -27,10 +26,19 @@ describe BitmapEditor do
 
     context "with a poorly formatted command" do
       xit "returns an informative error" do # speak to PM to determine desired behaviour
-        image = double :image
         expect(image).to receive(:create).with(height: "a", width: "a")
 
         BitmapEditor.new(image).run('I a a')
+      end
+    end
+  end
+
+  describe "L X Y C" do
+    context "with a well formatted command" do
+      it "colours the pixel at 1 1 black" do
+        expect(image).to receive(:add_colour).with(x: 1, y: 1, colour: "#000000")
+
+        BitmapEditor.new(image).run('L 1 1 #000000')
       end
     end
   end
